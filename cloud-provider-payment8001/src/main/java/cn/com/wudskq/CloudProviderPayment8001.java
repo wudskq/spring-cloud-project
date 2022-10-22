7b0fee52db4d94d2280c28043e56281fe5b805c2
chenfangchao <socrates11@foxmail.com>
2022-10-22 23:18:08 +0800

    [add]#添加支付模块

---
+++ cloud-provider-payment8001/src/main/java/cn/com/wudskq/CloudProviderPayment8001.java
@@ -0,0 +1,22 @@
+package cn.com.wudskq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenfangchao
+ * @title: CloudProviderPayment8001
+ * @projectName spring-cloud-project
+ * @description: TODO
+ * @date 2022/10/22 11:12 PM
+ */
+@Slf4j
+@SpringBootApplication
+public class CloudProviderPayment8001 {
+    public static void main(String[] args) {
+        log.info("-- CloudProviderPayment8001 启动中 --");
+        SpringApplication.run(CloudProviderPayment8001.class);
+        log.info("-- CloudProviderPayment8001 启动成功 --");
+    }
+}
