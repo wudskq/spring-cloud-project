d0aad4df1f7600a03e36677d6811c75c9a42dfcb
chenfangchao <socrates11@foxmail.com>
2022-10-24 10:36:35 +0800

    [add]#新增订单模块80-注册进入到zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/cn/com/wudskq/CloudConsumeZkrOrder80.java
@@ -0,0 +1,22 @@
+package cn.com.wudskq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenfangchao
+ * @title: CloudConsumerOrder80
+ * @projectName spring-cloud-project
+ * @description: TODO
+ * @date 2022/10/23 2:59 PM
+ */
+@Slf4j
+@SpringBootApplication
+public class CloudConsumeZkrOrder80 {
+    public static void main(String[] args) {
+        log.info("-- CloudConsumeZkrOrder80 启动中 --");
+        SpringApplication.run(CloudConsumeZkrOrder80.class);
+        log.info("-- CloudConsumeZkrOrder80 启动成功 --");
+    }
+}
