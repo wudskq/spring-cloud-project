95fa42034c423ef94ef4822b8e42fa082dfddbc9
chenfangchao <socrates11@foxmail.com>
2022-10-25 15:45:35 +0800

    [update]#支付服务8001模拟延迟

--- cloud-provider-payment8001/src/main/java/cn/com/wudskq/service/impl/PaymentServiceImpl.java
+++ cloud-provider-payment8001/src/main/java/cn/com/wudskq/service/impl/PaymentServiceImpl.java
@@ -4,6 +4,7 @@ import cn.com.wudskq.dto.Payment;
 import cn.com.wudskq.mapper.PaymentMapper;
 import cn.com.wudskq.service.PaymentService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ import java.util.List;
  * @description: TODO
  * @date 2022/10/23 1:15 AM
  */
+@Slf4j
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
@@ -26,6 +28,12 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public List<Payment> list() {
         QueryWrapper<Payment> queryWrapper = new QueryWrapper<>();
+        try {
+            Thread.sleep(3000);
+            log.debug("----支付服务延迟3秒----");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return paymentMapper.selectList(queryWrapper);
     }
 
