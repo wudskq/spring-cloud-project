ecb6e0edc58cb5481650487f396667ae2cd9c85d
chenfangchao <socrates11@foxmail.com>
2022-10-26 17:01:46 +0800

    [update]#支付服务8005添加hystrix-dashboard监测页面

--- cloud-provider-payment8005/src/main/java/cn/com/wudskq/CloudProviderPayment8005.java
+++ cloud-provider-payment8005/src/main/java/cn/com/wudskq/CloudProviderPayment8005.java
@@ -1,12 +1,19 @@
 package cn.com.wudskq;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.HystrixMetricsProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletRegistration;
 
 /**
  * @author chenfangchao
@@ -27,5 +34,21 @@ public class CloudProviderPayment8005 {
         log.info("-- CloudProviderPayment8005 启动中 --");
         SpringApplication.run(CloudProviderPayment8005.class);
         log.info("-- CloudProviderPayment8005 启动成功 --");
+    }
+
+    /**
+     * *此配置是为广限务监控而秘電，与服务容错本身无关，springcloud升级后的坑
+     * *servletRegistrationBean因springboot的默认路经不是"/hystrix.stream"，
+     * *只要在自己的项目里配置上下面用servlet就可以了
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<Servlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
