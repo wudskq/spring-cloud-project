0f181eb20642a8d38e5e5f4466c74cbf4d93fdc3
chenfangchao <socrates11@foxmail.com>
2022-10-28 17:40:37 +0800

    [add]#添加cloud-config-center微服务配置中心服务

---
+++ cloud-config-center3344/src/main/java/cn/com/wudskq/CloudConfigCenter3344.java
@@ -0,0 +1,18 @@
+package cn.com.wudskq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenfangchao
+ * @title: CloudConfigCenter3344
+ * @projectName spring-cloud-project
+ * @description: TODO
+ * @date 2022/10/28 5:34 PM
+ */
+@SpringBootApplication
+public class CloudConfigCenter3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigCenter3344.class);
+    }
+}
