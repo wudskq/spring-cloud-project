da7c90839726b3581b5590cb22ee51714a98104b
chenfangchao <socrates11@foxmail.com>
2022-10-30 23:48:30 +0800

    [add]#cloud-stream配置消费者组

--- cloud-stream-rabbitmq-consumer8803/src/main/java/cn/com/wudskq/CloudStreamRabbitmqConsumer8803.java
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/cn/com/wudskq/CloudStreamRabbitmqConsumer8803.java
@@ -1,11 +1,20 @@
 package cn.com.wudskq;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @author chenfangchao
- * @title: CloudStreamRabbitmqConsumer8802
+ * @title: CloudStreamRabbitmqConsumer8803
  * @projectName spring-cloud-project
  * @description: TODO
  * @date 2022/10/30 9:38 PM
  */
+@EnableEurekaClient
+@SpringBootApplication
 public class CloudStreamRabbitmqConsumer8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamRabbitmqConsumer8803.class);
+    }
 }
