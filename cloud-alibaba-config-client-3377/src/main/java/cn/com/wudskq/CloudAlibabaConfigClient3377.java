c9ffa8e8d233b8c8894720484b7b2a051f109aff
chenfangchao <socrates11@foxmail.com>
2022-11-06 17:14:40 +0800

    [add]#nacos配置中心

---
+++ cloud-alibaba-config-client-3377/src/main/java/cn/com/wudskq/CloudAlibabaConfigClient3377.java
@@ -0,0 +1,20 @@
+package cn.com.wudskq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author chenfangchao
+ * @title: CloudAlibabaConfigClient3377
+ * @projectName spring-cloud-project
+ * @description: TODO
+ * @date 2022/11/6 4:56 PM
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudAlibabaConfigClient3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaConfigClient3377.class);
+    }
+}
